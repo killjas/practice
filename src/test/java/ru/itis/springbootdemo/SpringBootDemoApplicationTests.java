4767b218867d64adb5722e701aa0c2d56d0a62fc
baggio1103 <46907001+baggio1103@users.noreply.github.com>
2020-07-17 17:30:48 +0300

    Update SpringBootDemoApplicationTests.java

--- src/test/java/ru/itis/springbootdemo/SpringBootDemoApplicationTests.java
+++ src/test/java/ru/itis/springbootdemo/SpringBootDemoApplicationTests.java
@@ -9,6 +9,23 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringBootDemoApplicationTests {
 
+    @Autowired
+    private UserRequestRepository requestRepository;
+
+    @Autowired
+    private UsersRequestService usersRequestService;
+
+    @Test
+    public void testSaveRequest() {
+        LocalDateTime date = LocalDateTime.now()
+                .with(TemporalAdjusters.firstDayOfMonth());
+        int previousSize = requestRepository.getAllByDateAfter(date).size();
+        usersRequestService.saveRequest("https://developer.mozilla.org", "192.168.5.255");
+        int currentSize = requestRepository.getAllByDateAfter(date).size();
+        assertThat(currentSize).isEqualTo(previousSize+1);
+    }
+
+    
     @Test
     public void contextLoads() {
     }
